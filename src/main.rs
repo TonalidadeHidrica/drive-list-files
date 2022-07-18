ac5e9c6b459c0e8688040275b96afc9e127aeee8
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2022-07-18 18:48:51 +0900

    Get file list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use google_drive3::{
+    hyper,
+    hyper_rustls::HttpsConnectorBuilder,
+    oauth2::{self, InstalledFlowAuthenticator, InstalledFlowReturnMethod},
+    DriveHub,
+};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let client = hyper::Client::builder().build(
+        HttpsConnectorBuilder::new()
+            .with_native_roots()
+            .https_or_http()
+            .enable_http1()
+            .enable_http2()
+            .build(),
+    );
+    let secret = oauth2::read_application_secret(".google/client_secret.json").await?;
+    let auth = InstalledFlowAuthenticator::builder(secret, InstalledFlowReturnMethod::HTTPRedirect)
+        .persist_tokens_to_disk(".google/tokens.json")
+        .build()
+        .await
+        .unwrap();
+    let drive = DriveHub::new(client, auth);
+
+    let res = drive.files().list().doit().await;
+    println!("{:?}", res);
+
+    Ok(())
 }
